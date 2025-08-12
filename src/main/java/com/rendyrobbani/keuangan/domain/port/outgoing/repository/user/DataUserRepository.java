0f690bad1589cf569be5784e95feab1fefa6b44d
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-12 15:38:34 +0800

    Dev

--- src/main/java/com/rendyrobbani/keuangan/domain/port/outgoing/repository/user/DataUserRepository.java
+++ src/main/java/com/rendyrobbani/keuangan/domain/port/outgoing/repository/user/DataUserRepository.java
@@ -1,8 +1,10 @@
 package com.rendyrobbani.keuangan.domain.port.outgoing.repository.user;
 
 import com.rendyrobbani.keuangan.domain.model.entity.user.DataUser;
+import com.rendyrobbani.keuangan.domain.port.outgoing.repository.CreateRepository;
 import com.rendyrobbani.keuangan.domain.port.outgoing.repository.SelectRepository;
 
-public interface DataUserRepository extends SelectRepository<String, DataUser> {
+public interface DataUserRepository extends SelectRepository<String, DataUser>,
+                                            CreateRepository<DataUser> {
 
 }
--- src/main/java/com/rendyrobbani/keuangan/infrastructure/persistence/repository/adapter/user/DataUserRepositoryImpl.java
+++ src/main/java/com/rendyrobbani/keuangan/infrastructure/persistence/repository/adapter/user/DataUserRepositoryImpl.java
@@ -5,13 +5,15 @@ import com.rendyrobbani.keuangan.domain.port.outgoing.repository.user.DataUserRe
 import com.rendyrobbani.keuangan.infrastructure.persistence.entity.adapter.user.DataUserEntity;
 import com.rendyrobbani.keuangan.infrastructure.persistence.mapper.BaseMapper;
 import com.rendyrobbani.keuangan.infrastructure.persistence.mapper.user.DataUserEntityMapper;
+import com.rendyrobbani.keuangan.infrastructure.persistence.repository.operation.CreateRepositoryOperation;
 import com.rendyrobbani.keuangan.infrastructure.persistence.repository.operation.SelectRepositoryOperation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DataUserRepositoryImpl implements DataUserRepository,
-                                               SelectRepositoryOperation<String, DataUser, DataUserEntity> {
+                                               SelectRepositoryOperation<String, DataUser, DataUserEntity>,
+                                               CreateRepositoryOperation<String, DataUser, DataUserEntity> {
 
 	private final DataUserJpaRepository repository;
 
